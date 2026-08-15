931d0d935f5c0d9fb36fe03802291cbe2e925342
magcicada <l1334938030@gmail.com>
2026-08-16 00:55:06 +0800

    fix aae crash

--- src/main/java/lu/kolja/expandedae/xmod/advancedae/AdvancedAE.java
+++ src/main/java/lu/kolja/expandedae/xmod/advancedae/AdvancedAE.java
@@ -5,6 +5,10 @@ import appeng.api.networking.crafting.ICraftingCPU;
 import appeng.api.upgrades.Upgrades;
 import lu.kolja.expandedae.mixin.compat.advancedae.AAEAccessorAdvCraftingCPULogic;
 import lu.kolja.expandedae.mixin.compat.advancedae.AAEAccessorExecutingCraftingJob;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.pedroksl.advanced_ae.common.cluster.AdvCraftingCPU;
 import net.pedroksl.advanced_ae.common.definitions.AAEBlocks;
 import net.pedroksl.advanced_ae.common.definitions.AAEItems;
@@ -13,11 +17,45 @@ import static lu.kolja.expandedae.definition.ExpItems.AUTO_COMPLETE_CARD;
 
 public class AdvancedAE {
     public AdvancedAE() {
-        Upgrades.add(AUTO_COMPLETE_CARD, AAEItems.SMALL_ADV_PATTERN_PROVIDER, 1, "group.adv_pattern_provider.name");
-        Upgrades.add(AUTO_COMPLETE_CARD, AAEItems.ADV_PATTERN_PROVIDER, 1, "group.advanced_pattern_provider.name");
+        Upgrades.add(
+                AUTO_COMPLETE_CARD,
+                getAdvancedAEItem("small_adv_pattern_provider_part"),
+                1,
+                "group.adv_pattern_provider.name"
+        );
 
-        Upgrades.add(AUTO_COMPLETE_CARD, AAEBlocks.SMALL_ADV_PATTERN_PROVIDER, 1, "group.adv_pattern_provider.name");
-        Upgrades.add(AUTO_COMPLETE_CARD, AAEBlocks.ADV_PATTERN_PROVIDER, 1, "group.advanced_pattern_provider.name");
+        Upgrades.add(
+                AUTO_COMPLETE_CARD,
+                getAdvancedAEItem("adv_pattern_provider_part"),
+                1,
+                "group.advanced_pattern_provider.name"
+        );
+
+        Upgrades.add(
+                AUTO_COMPLETE_CARD,
+                getAdvancedAEBlock("small_adv_pattern_provider"),
+                1,
+                "group.adv_pattern_provider.name"
+        );
+
+        Upgrades.add(
+                AUTO_COMPLETE_CARD,
+                getAdvancedAEBlock("adv_pattern_provider"),
+                1,
+                "group.advanced_pattern_provider.name"
+        );
+    }
+
+    private static Item getAdvancedAEItem(String id) {
+        return BuiltInRegistries.ITEM.get(
+                new ResourceLocation("advanced_ae", id)
+        );
+    }
+
+    private static Block getAdvancedAEBlock(String id) {
+        return BuiltInRegistries.BLOCK.get(
+                new ResourceLocation("advanced_ae", id)
+        );
     }
 
     public static void handleCpu(ICraftingCPU cpu, IPatternDetails details) {
